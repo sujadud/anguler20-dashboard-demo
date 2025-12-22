9da8590a61b2bbe5e85f6423bd32315d3133a490
MD. Sujad UD <43004887+sujadud@users.noreply.github.com>
2025-12-23 01:22:06 +0600

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component, signal } from '@angular/core';
-import { RouterOutlet, ɵEmptyOutletComponent } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CommonModule, ɵEmptyOutletComponent],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
